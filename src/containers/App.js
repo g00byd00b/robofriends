6e057105f8ec0b4e1f379c1bd5773f2bb206ae8d
g00byd00b <64923238+g00byd00b@users.noreply.github.com>
2023-02-12 07:39:28 -0700

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -49,33 +49,3 @@ class App extends Component {
 
 
 export default App;
-// class App extends Component {
-//     constructor() {
-//         super()
-//         this.state = {
-//             robots: robots,
-//             searchfield: ''
-//         }
-//     }
-  
-//     onSearchChange = (event) => {
-//         console.log(event.target.value);
-//        this.setState({ searchfield: event.target.value })
-//     }
-
-//     render() {
-//         const filteredRobots = this.state.robots.filter(robots =>{
-//             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-//         }) 
-//         return (
-//             <div className='tc'>
-//                 <h1>RoboFranz</h1>
-//                 <SearchBox searchChange ={this.onSearchChange}/>
-//                 <CardList robots={filteredRobots} />
-//             </div>
-
-//     );
-//     }  
-// }
-
-// export default App;
